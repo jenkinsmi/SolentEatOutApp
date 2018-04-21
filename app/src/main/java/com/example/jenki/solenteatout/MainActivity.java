0e33b6f5acb2e78d4b1fb9512cc90951a98279da
Michael Jenkins <jenkinsmi@hotmail.com>
2018-04-21 18:55:11 +0100

    Added Map GPS but it shows the sea for some reason

--- app/src/main/java/com/example/jenki/solenteatout/MainActivity.java
+++ app/src/main/java/com/example/jenki/solenteatout/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jenki.solenteatout;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,33 +8,60 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.location.Location;
+import android.content.Context;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements LocationListener
 {
 
     MapView mv;
-
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-
         super.onCreate(savedInstanceState);
-
-        // This line sets the user agent, a requirement to download OSM maps
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-
         setContentView(R.layout.activity_main);
+
+        LocationManager mgr=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
 
         mv = (MapView)findViewById(R.id.map1);
 
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(16);
-        mv.getController().setCenter(new GeoPoint(51.05,-0.72));
+    }
+
+    public void onLocationChanged(Location newLoc)
+    {
+        double latitude = newLoc.getLatitude();
+        double longitude = newLoc.getLongitude();
+
+        mv.getController().setCenter(new GeoPoint(latitude,longitude));
+    }
+
+    public void onProviderDisabled(String provider)
+    {
+        Toast.makeText(this, "Provider " + provider +
+                " disabled", Toast.LENGTH_LONG).show();
+    }
+
+    public void onProviderEnabled(String provider)
+    {
+        Toast.makeText(this, "Provider " + provider +
+                " enabled", Toast.LENGTH_LONG).show();
+    }
+
+    public void onStatusChanged(String provider,int status,Bundle extras)
+    {
+
+        Toast.makeText(this, "Status changed: " + status,
+                Toast.LENGTH_LONG).show();
     }
 }
